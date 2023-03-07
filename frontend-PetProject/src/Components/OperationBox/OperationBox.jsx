7469ef3dfdd4bc59f14fa1f397b54da63d0780a9
mezeipb <mezei.peter.barnabas@gmail.com>
2023-03-07 21:41:09 +0100

    Create basic operation box structure and style

--- frontend-PetProject/src/Components/OperationBox/OperationBox.jsx
+++ frontend-PetProject/src/Components/OperationBox/OperationBox.jsx
@@ -2,7 +2,28 @@ import './OperationBox.css';
 
 function OperationBox() {
     return(
-        <div id="component-operation-box">Operation box</div>
+        <div id="component-operation-box">
+            <div className="operation-box">
+                <nav id="ob-options">
+                    <button className="ob-option">Add</button>
+                    <button className="ob-option">Remove</button>
+                    <button className="ob-option">Find</button>
+                    <button className="ob-option">Update</button>
+                </nav>
+                <div id="ob-selected-option">
+                    <button className="ob-side-arrow">&#60;</button>
+                    <form id="ob-form" onSubmit={event => {
+                        event.preventDefault();
+                    }}>
+                        <h2>Add pet</h2>
+                        <input type="text" name="ob-form-name" id="ob-form-name" placeholder="name"/>
+                        <input type="text" name="ob-form-weight" id="ob-form-weight" placeholder="weight"/>
+                        <button>Add</button>
+                    </form>
+                    <button className="ob-side-arrow">&#62;</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
