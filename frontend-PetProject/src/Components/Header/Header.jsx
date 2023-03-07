d9b50e1d354b7168ad743afc545af146f7853b64
mezeipb <mezei.peter.barnabas@gmail.com>
2023-03-07 20:29:17 +0100

    Add header title and reset icon

--- frontend-PetProject/src/Components/Header/Header.jsx
+++ frontend-PetProject/src/Components/Header/Header.jsx
@@ -2,7 +2,10 @@ import './Header.css';
 
 function Header() {
     return (
-        <div id="component-header">Header</div>
+        <div id="component-header">
+            <h1>Pet Project</h1>
+            <span class="material-symbols-outlined">device_reset</span>
+        </div>
     );
 }
 
