f146a265692310f38356d198185bb41d78c1fa8d
mezeipb <mezei.peter.barnabas@gmail.com>
2023-03-08 21:04:09 +0100

    Add base pet tree structure

--- frontend-PetProject/src/Components/PetTree/PetTree.jsx
+++ frontend-PetProject/src/Components/PetTree/PetTree.jsx
@@ -3,8 +3,80 @@ import './PetTree.css';
 function PetTree() {
     return (
         <div id="component-pet-tree">
-            <p className='p'>Pet tree</p>
-            <h2 className='h2'>Test</h2>
+            <div className="inner-wrapper">
+                <ul>
+                    <li>
+                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>name</p><p>10kg</p></div>
+                        <ul>
+                            <li>
+                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>asd</p><p>10kg</p></div>
+                                <ul>
+                                    <li>
+                                        <div className="invisible">asd</div>
+                                    </li>
+                                    <li>
+                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                <ul>
+                                    <li>
+                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                        <ul>
+                                            <li>
+                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                <ul>
+                                                    <li>
+                                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                        <ul>
+                                            <li>
+                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                            </li>
+                                            <li>
+                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                <ul>
+                                                    <li>
+                                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                        <ul>
+                                                            <li>
+                                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                            </li>
+                                                            <li>
+                                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+                                                    <li>
+                                                        <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                        <ul>
+                                                            <li>
+                                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                            </li>
+                                                            <li>
+                                                                <div><img src="https://cdn.pixabay.com/photo/2013/07/13/13/41/pig-161381_960_720.png" alt="animal portrait" /><p>qwe</p><p>10kg</p></div>
+                                                            </li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>  
+                            </li>
+                        </ul>
+                                    
+                    </li>
+                </ul>
+            </div>   
         </div>
     );
 }
