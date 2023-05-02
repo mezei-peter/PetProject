3d6ea36b6799fe03957dc2605d48d92c50cbeca7
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-02 16:30:44 +0200

    Plan rendering pet-to-remove options

--- frontend-PetProject/src/Components/OperationBox/RemoveForm.jsx
+++ frontend-PetProject/src/Components/OperationBox/RemoveForm.jsx
@@ -1,7 +1,13 @@
+import {useRef} from "react";
+
 function RemoveForm() {
+    const petToRemove = useRef("");
 
     const handleSubmit = () => {
-
+        if (!petToRemove.current) {
+            return;
+        }
+        //TODO
     }
 
     return (
@@ -11,8 +17,11 @@ function RemoveForm() {
         }}>
             <h2>Remove pet</h2>
             <label htmlFor="pets-select">Pet to remove: </label>
-            <select id="pet-select">
+            <select name="pet-to-remove" id="pet-select"
+                    onChange={event => petToRemove.current = event.target.value}>
                 <option value="">Select pet</option>
+                <option value="TEST">TEST</option>
+                //TODO: Render all pet names and uuids here
             </select>
             <button>Delete</button>
         </form>
