181d4aa838a002fcb6ba6e9f9b5f49975bbf8c7c
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-02 16:02:48 +0200

    Add 'Remove' form design

--- frontend-PetProject/src/Components/OperationBox/RemoveForm.jsx
+++ frontend-PetProject/src/Components/OperationBox/RemoveForm.jsx
@@ -1,6 +1,21 @@
 function RemoveForm() {
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-      <div>TEST REMOVE FORM</div>
+        <form id="ob-form" onSubmit={event => {
+            event.preventDefault();
+            handleSubmit();
+        }}>
+            <h2>Remove pet</h2>
+            <label htmlFor="pets-select">Pet to remove: </label>
+            <select id="pet-select">
+                <option value="">Select pet</option>
+            </select>
+            <button>Delete</button>
+        </form>
     );
 }
 
