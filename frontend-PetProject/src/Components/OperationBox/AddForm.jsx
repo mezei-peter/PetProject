ebfb2bd722606c05bcbe939610e07a44306a3d29
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-09 16:04:33 +0200

    Format AddForm.jsx

--- frontend-PetProject/src/Components/OperationBox/AddForm.jsx
+++ frontend-PetProject/src/Components/OperationBox/AddForm.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function AddForm({setTreeRoot}) {
     const [name, setName] = useState("");
@@ -42,7 +42,8 @@ function AddForm({setTreeRoot}) {
             handleSubmit();
         }}>
             <h2>Add pet</h2>
-            <input type="text" name="ob-form-name" id="ob-form-name" placeholder="name" maxLength="10" required value={name}
+            <input type="text" name="ob-form-name" id="ob-form-name" placeholder="name" maxLength="10" required
+                   value={name}
                    onChange={event => setName(event.target.value)}/>
             <input type="number" min="1" max="9999999" step="1" name="ob-form-weight" id="ob-form-weight"
                    placeholder="weight"
